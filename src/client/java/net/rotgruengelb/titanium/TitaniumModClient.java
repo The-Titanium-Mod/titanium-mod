b30fb8047b3097492eedd2261d490606ac7fc84b
Daniel (aka. rotgruengelb) <rotgrungelblab@gmail.com>
2025-03-23 15:41:29 +0100

    chore(stonecutter): setup stonecutter

---
+++ src/client/java/net/rotgruengelb/titanium/TitaniumModClient.java
@@ -0,0 +1,9 @@
+package net.rotgruengelb.titanium;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TitaniumModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+	}
+}
---
+++ src/main/java/net/rotgruengelb/titanium/TitaniumMod.java
@@ -0,0 +1,21 @@
+package net.rotgruengelb.titanium;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TitaniumMod implements ModInitializer {
+	public static final String MOD_ID = "titanium";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final String VERSION = /*$ mod_version*/ "0.1.0";
+
+	public static Identifier id(String path) {
+		return Identifier.of(MOD_ID, path);
+	}
+
+	@Override
+	public void onInitialize() {
+	}
+
+}
