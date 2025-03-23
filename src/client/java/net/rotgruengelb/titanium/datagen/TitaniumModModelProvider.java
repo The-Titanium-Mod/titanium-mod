52703dbf6429ed2c01b568259d788adb3112f664
Steaf23 <steafmobile@gmail.com>
2025-03-24 00:12:43 +0100

    feat(blocks): added lang file, added all sod variants

--- src/client/java/net/rotgruengelb/titanium/datagen/TitaniumModModelProvider.java
+++ src/client/java/net/rotgruengelb/titanium/datagen/TitaniumModModelProvider.java
@@ -18,8 +18,24 @@ public class TitaniumModModelProvider extends FabricModelProvider
     @Override
     public void generateBlockStateModels(BlockStateModelGenerator generator) {
         generator.registerSimpleCubeAll(TitaniumModBlocks.CLART);
-        generator.registerSingleton(TitaniumModBlocks.SOD, TextureMap.sideAndTop(TitaniumModBlocks.SOD).put(TextureKey.BOTTOM, TextureMap.getId(TitaniumModBlocks.CLART)), Models.CUBE_BOTTOM_TOP);
-        generator.registerSingleton(TitaniumModBlocks.SOD_BATCH, TextureMap.all(TextureMap.getSubId(TitaniumModBlocks.SOD, "_top")), Models.CUBE_ALL);
+        generator.registerSingleton(TitaniumModBlocks.SOD,
+                TextureMap.sideAndTop(TitaniumModBlocks.SOD).put(TextureKey.BOTTOM, TextureMap.getId(TitaniumModBlocks.CLART)),
+                Models.CUBE_BOTTOM_TOP);
+        generator.registerSingleton(TitaniumModBlocks.SOD_BATCH,
+                TextureMap.all(TextureMap.getSubId(TitaniumModBlocks.SOD, "_top")),
+                Models.CUBE_ALL);
+
+        generator.registerSimpleCubeAll(TitaniumModBlocks.VEINY_CLART);
+        generator.registerSingleton(TitaniumModBlocks.VEINY_SOD,
+                TextureMap.sideAndTop(TitaniumModBlocks.VEINY_SOD).put(TextureKey.BOTTOM, TextureMap.getId(TitaniumModBlocks.VEINY_CLART)),
+                Models.CUBE_BOTTOM_TOP);
+
+        generator.registerSingleton(TitaniumModBlocks.ROTTEN_SOD,
+                TextureMap.sideAndTop(TitaniumModBlocks.ROTTEN_SOD).put(TextureKey.BOTTOM, TextureMap.getId(TitaniumModBlocks.CLART)),
+                Models.CUBE_BOTTOM_TOP);
+        generator.registerSingleton(TitaniumModBlocks.ROTTEN_SOD_BATCH,
+                TextureMap.all(TextureMap.getSubId(TitaniumModBlocks.ROTTEN_SOD, "_top")),
+                Models.CUBE_ALL);
     }
 
     @Override
--- src/main/java/net/rotgruengelb/titanium/registry/TitaniumModBlocks.java
+++ src/main/java/net/rotgruengelb/titanium/registry/TitaniumModBlocks.java
@@ -15,12 +15,24 @@ import java.util.function.Function;
 public class TitaniumModBlocks
 {
     public static Block CLART = register("clart", Block::new, AbstractBlock.Settings.create()
-            .sounds(BlockSoundGroup.GRAVEL));
+            .sounds(BlockSoundGroup.ROOTED_DIRT));
 
     public static Block SOD = register("sod", Block::new, AbstractBlock.Settings.create()
             .sounds(BlockSoundGroup.NYLIUM));
 
     public static Block SOD_BATCH = register("sod_batch", Block::new, AbstractBlock.Settings.create()
+            .sounds(BlockSoundGroup.GRASS));
+
+    public static Block VEINY_CLART = register("veiny_clart", Block::new, AbstractBlock.Settings.create()
+            .sounds(BlockSoundGroup.ROOTED_DIRT));
+
+    public static Block VEINY_SOD = register("veiny_sod", Block::new, AbstractBlock.Settings.create()
+            .sounds(BlockSoundGroup.NYLIUM));
+
+    public static Block ROTTEN_SOD = register("rotten_sod", Block::new, AbstractBlock.Settings.create()
+            .sounds(BlockSoundGroup.NYLIUM));
+
+    public static Block ROTTEN_SOD_BATCH = register("rotten_sod_batch", Block::new, AbstractBlock.Settings.create()
             .sounds(BlockSoundGroup.GRASS));
 
     protected static Block register(String name, Function<AbstractBlock.Settings, Block> factory, AbstractBlock.Settings settings) {
