8c35300921d60a4ab3e96b779e60713acda13272
Daniel (aka. rotgruengelb) <rotgrungelblab@gmail.com>
2025-03-24 02:25:27 +0100

    refactor(datagen): use the extension pattern to make TitaniumModModelProvider more uniform

--- src/client/java/net/rotgruengelb/titanium/datagen/TitaniumModModelProvider.java
+++ src/client/java/net/rotgruengelb/titanium/datagen/TitaniumModModelProvider.java
@@ -1,45 +1,53 @@
 package net.rotgruengelb.titanium.datagen;
 
+import com.google.gson.JsonElement;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
-import net.minecraft.data.client.BlockStateModelGenerator;
-import net.minecraft.data.client.ItemModelGenerator;
-import net.minecraft.data.client.Models;
-import net.minecraft.data.client.TextureKey;
-import net.minecraft.data.client.TextureMap;
+import net.minecraft.block.Block;
+import net.minecraft.data.client.*;
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
 import net.rotgruengelb.titanium.registry.TitaniumModBlocks;
 
-public class TitaniumModModelProvider extends FabricModelProvider
-{
-    public TitaniumModModelProvider(FabricDataOutput output) {
-        super(output);
-    }
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
-    @Override
-    public void generateBlockStateModels(BlockStateModelGenerator generator) {
-        generator.registerSimpleCubeAll(TitaniumModBlocks.CLART);
-        generator.registerSingleton(TitaniumModBlocks.SOD,
-                TextureMap.sideAndTop(TitaniumModBlocks.SOD).put(TextureKey.BOTTOM, TextureMap.getId(TitaniumModBlocks.CLART)),
-                Models.CUBE_BOTTOM_TOP);
-        generator.registerSingleton(TitaniumModBlocks.SOD_BATCH,
-                TextureMap.all(TextureMap.getSubId(TitaniumModBlocks.SOD, "_top")),
-                Models.CUBE_ALL);
+public class TitaniumModModelProvider extends FabricModelProvider {
+	public TitaniumModModelProvider(FabricDataOutput output) {
+		super(output);
+	}
 
-        generator.registerSimpleCubeAll(TitaniumModBlocks.VEINY_CLART);
-        generator.registerSingleton(TitaniumModBlocks.VEINY_SOD,
-                TextureMap.sideAndTop(TitaniumModBlocks.VEINY_SOD).put(TextureKey.BOTTOM, TextureMap.getId(TitaniumModBlocks.VEINY_CLART)),
-                Models.CUBE_BOTTOM_TOP);
+	@Override
+	public void generateBlockStateModels(BlockStateModelGenerator baseGenerator) {
+		TitaniumBlockStateModelGenerator generator = (TitaniumBlockStateModelGenerator) baseGenerator;
 
-        generator.registerSingleton(TitaniumModBlocks.ROTTEN_SOD,
-                TextureMap.sideAndTop(TitaniumModBlocks.ROTTEN_SOD).put(TextureKey.BOTTOM, TextureMap.getId(TitaniumModBlocks.CLART)),
-                Models.CUBE_BOTTOM_TOP);
-        generator.registerSingleton(TitaniumModBlocks.ROTTEN_SOD_BATCH,
-                TextureMap.all(TextureMap.getSubId(TitaniumModBlocks.ROTTEN_SOD, "_top")),
-                Models.CUBE_ALL);
-    }
+		generator.registerSimpleCubeAll(TitaniumModBlocks.CLART);
+		generator.registerSod(TitaniumModBlocks.SOD, TitaniumModBlocks.CLART);
+		generator.registerSodBatch(TitaniumModBlocks.SOD_BATCH, TitaniumModBlocks.SOD);
+		generator.registerSimpleCubeAll(TitaniumModBlocks.VEINY_CLART);
+		generator.registerSod(TitaniumModBlocks.VEINY_SOD, TitaniumModBlocks.VEINY_CLART);
+		generator.registerSodBatch(TitaniumModBlocks.ROTTEN_SOD, TitaniumModBlocks.CLART);
+		generator.registerSodBatch(TitaniumModBlocks.ROTTEN_SOD_BATCH, TitaniumModBlocks.ROTTEN_SOD);
+	}
 
-    @Override
-    public void generateItemModels(ItemModelGenerator generator) {
+	@Override
+	public void generateItemModels(ItemModelGenerator generator) {
 
-    }
+	}
+
+	private static class TitaniumBlockStateModelGenerator extends BlockStateModelGenerator {
+		public TitaniumBlockStateModelGenerator(Consumer<BlockStateSupplier> blockStateCollector, BiConsumer<Identifier, Supplier<JsonElement>> modelCollector, Consumer<Item> simpleItemModelExemptionCollector) {
+			super(blockStateCollector, modelCollector, simpleItemModelExemptionCollector);
+		}
+
+		public void registerSod(Block block, Block from) {
+			this.registerSingleton(block, TextureMap.sideAndTop(block)
+					.put(TextureKey.BOTTOM, TextureMap.getId(from)), Models.CUBE_BOTTOM_TOP);
+		}
+
+		public void registerSodBatch(Block block, Block from) {
+			this.registerSingleton(block, TextureMap.all(TextureMap.getSubId(from, "_top")), Models.CUBE_ALL);
+		}
+	}
 }
