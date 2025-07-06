2735bdfd67b859f271e6b5ff8f9db17b5a458981
Daniel (rotgruengelb) <rotgruengelb@rotgruengelb.net>
2025-07-06 23:24:02 +0200

    refactor:  simplify block type registration and rename biome register method (#45)
    * feat(biomes): add Wildwood Wastes biome
    
    * refactor: rename "register" method to "biome" for consistency
    
    * refactor: simplify WoodType and BlockSetType registration using fabric api object builders

--- src/main/java/net/rotgruengelb/titanium/block/TitaniumBlockTypes.java
+++ src/main/java/net/rotgruengelb/titanium/block/TitaniumBlockTypes.java
@@ -1,26 +1,27 @@
 package net.rotgruengelb.titanium.block;
 
+import net.fabricmc.fabric.api.object.builder.v1.block.type.BlockSetTypeBuilder;
+import net.fabricmc.fabric.api.object.builder.v1.block.type.WoodTypeBuilder;
 import net.minecraft.block.BlockSetType;
 import net.minecraft.block.WoodType;
+import net.rotgruengelb.titanium.Titanium;
 
 public class TitaniumBlockTypes {
 
-	public static class TitaniumWoodTypes {
-		public static final WoodType WILDWOOD = register(new WoodType("titanium:wildwood",
-				TitaniumBlockTypes.TitaniumBlockSetTypes.WILDWOOD));
+    public static class TitaniumWoodTypes {
+        public static final WoodType WILDWOOD = woodType("wildwood", TitaniumBlockSetTypes.WILDWOOD, new WoodTypeBuilder());
 
-		private static WoodType register(WoodType type) {
-			WoodType.VALUES.put(type.name(), type);
-			return type;
-		}
-	}
+        private static WoodType woodType(String name, BlockSetType blockSetType, WoodTypeBuilder woodTypeBuilder) {
+            return woodTypeBuilder.register(Titanium.id(name), blockSetType);
+        }
+    }
 
-	public static class TitaniumBlockSetTypes {
-		public static final BlockSetType WILDWOOD = register(new BlockSetType("titanium:wildwood"));
+    public static class TitaniumBlockSetTypes {
 
-		private static BlockSetType register(BlockSetType blockSetType) {
-			BlockSetType.VALUES.put(blockSetType.name, blockSetType);
-			return blockSetType;
-		}
-	}
+        public static final BlockSetType WILDWOOD = blockSet("wildwood", new BlockSetTypeBuilder());
+
+        private static BlockSetType blockSet(String name, BlockSetTypeBuilder blockSetTypeBuilder) {
+            return blockSetTypeBuilder.register(Titanium.id(name));
+        }
+    }
 }
