8a4362dae767fc463b9069a138d515f3263f29de
Daniel (rotgruengelb) <rotgruengelb@rotgruengelb.net>
2025-07-27 00:01:35 +0200

    refactor(util): enhance string conversion methods with detailed documentation

--- src/main/java/net/rotgruengelb/titanium/util/Util.java
+++ src/main/java/net/rotgruengelb/titanium/util/Util.java
@@ -6,8 +6,14 @@ import java.util.Arrays;
 
 public class Util {
 
-	public static @NotNull String snakeCaseToTitleCase(@NotNull String snakeCaseText) {
-		String[] words = snakeCaseText.split("_");
+    /**
+     * Converts a snake_case titleCaseString to Title Case.
+     *
+     * @param snakeCaseString the snake_case titleCaseString to convert
+     * @return the converted Title Case titleCaseString
+     */
+	public static @NotNull String snakeCaseToTitleCase(@NotNull String snakeCaseString) {
+		String[] words = snakeCaseString.split("_");
 		StringBuilder titleCaseText = new StringBuilder();
 
 		for (String word : words) {
@@ -22,10 +28,19 @@ public class Util {
 				.trim();
 	}
 
-	public static @NotNull String snakeCaseWithOfNotation(@NotNull String string) {
-		String[] words = string.split(" ");
+    /**
+     * Converts a Title Case titleCaseString into a Title Case titleCaseString with "of" Notation
+     * For example, {@code "Iron Block"} becomes {@code "Block of Iron"}.
+     * <p>
+     * If the input titleCaseString contains fewer than two words, the original titleCaseString is returned unchanged.
+     *
+     * @param titleCaseString the Title Case titleCaseString to convert
+     * @return the converted Title Case titleCaseString now with "of" Notation
+     */
+	public static @NotNull String titleCaseWithOfNotation(@NotNull String titleCaseString) {
+		String[] words = titleCaseString.split(" ");
 		if (words.length < 2) {
-			return string;
+			return titleCaseString;
 		}
 
 		String lastWord = words[words.length - 1]; // Get last word
@@ -34,7 +49,7 @@ public class Util {
 		return lastWord + " of " + rest;
 	}
 
-	public static String tryGetTranslationKey(Object object) {
+	public static String tryGetTranslationKey(Object object) throws RuntimeException {
 		try {
 			return object.getClass()
 					.getMethod("getTranslationKey")
